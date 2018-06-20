4b04b69ad014a97cd5c3b4784c583627dcb555bb
dlbaya-ay <dlbaya-ay@JF609A-029>
2018-06-20 14:18:17 +0800

    updated

--- MainSystem/MainSystem/frmAccount.Designer.cs
+++ MainSystem/MainSystem/frmAccount.Designer.cs
@@ -28,66 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.btnSearch = new System.Windows.Forms.Button();
-            this.lblStudentDetails = new System.Windows.Forms.Label();
+            this.grpStudentDetails = new System.Windows.Forms.GroupBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.grpStudentDetails.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(12, 93);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(776, 137);
-            this.panel1.TabIndex = 0;
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(561, 66);
+            this.txtSearch.Location = new System.Drawing.Point(641, 62);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(146, 20);
+            this.txtSearch.Size = new System.Drawing.Size(231, 20);
             this.txtSearch.TabIndex = 1;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(713, 66);
+            this.btnSearch.Location = new System.Drawing.Point(878, 62);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(75, 20);
             this.btnSearch.TabIndex = 3;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
             // 
-            // lblStudentDetails
+            // grpStudentDetails
             // 
-            this.lblStudentDetails.AutoSize = true;
-            this.lblStudentDetails.Location = new System.Drawing.Point(28, 93);
-            this.lblStudentDetails.Name = "lblStudentDetails";
-            this.lblStudentDetails.Size = new System.Drawing.Size(79, 13);
-            this.lblStudentDetails.TabIndex = 0;
-            this.lblStudentDetails.Text = "Student Details";
+            this.grpStudentDetails.Controls.Add(this.lblID);
+            this.grpStudentDetails.Location = new System.Drawing.Point(12, 274);
+            this.grpStudentDetails.Name = "grpStudentDetails";
+            this.grpStudentDetails.Size = new System.Drawing.Size(940, 106);
+            this.grpStudentDetails.TabIndex = 4;
+            this.grpStudentDetails.TabStop = false;
+            this.grpStudentDetails.Text = "Student Details";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(17, 31);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(941, 180);
+            this.dataGridView1.TabIndex = 5;
             // 
             // frmAccount
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblStudentDetails);
+            this.ClientSize = new System.Drawing.Size(964, 683);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.grpStudentDetails);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtSearch);
-            this.Controls.Add(this.panel1);
             this.Name = "frmAccount";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Account Management";
+            this.grpStudentDetails.ResumeLayout(false);
+            this.grpStudentDetails.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox txtSearch;
         private System.Windows.Forms.Button btnSearch;
-        private System.Windows.Forms.Label lblStudentDetails;
+        private System.Windows.Forms.GroupBox grpStudentDetails;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
